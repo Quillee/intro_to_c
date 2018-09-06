964bda6dd463b9a298f0c2a58a58bb66298faf7e
Melvin Peralta <peraltamdb@gmail.com>
2018-09-06 19:34:35 -0400

    sdl tutorial added

---
+++ sdl_tutorial/01_hello_SDL.cpp
@@ -0,0 +1,16 @@
+/* BY: Melvin Peralta
+ * Tutorial: Lazy foo SDL tutorial
+ * there will most likely be differences between tutorial code
+ *  and my code
+ */
+#include <SDL2/SDL.h>
+#include <stdio.h>
+
+int main(int argc, char *args[]){
+    // init a window to null
+    SDL_Window *window = nullptr;
+    // surface? w/e that is also init to null
+    SDL_Surface *screenSurface = nullptr;
+
+    
+}
