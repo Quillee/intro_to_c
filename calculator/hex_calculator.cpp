a316d4889184ef7f9f52aaf63497bcc32e41d61c
Melvin Peralta <peraltamdb@gmail.com>
2018-09-08 09:26:03 -0400

    changing environment from ubuntu mate to Arch

--- calculator/hex_calculator.cpp
+++ calculator/hex_calculator.cpp
@@ -8,10 +8,33 @@
  * 
  * ?>   hope to work with regular numbers too?
  */ 
-int main(int *argc, char **argv){
-    // might want to add args?
-    if(*argc > 3){
-        printf(argv[1]);
-    }
+
+void opt_parser(char **args);
+bool menu();
+void continuous_calculate();
+int main(int *argc, char **argv){    
+    opt_parser(argv)
     
+    
+}
+
+void opt_parser(char **args){
+    if(*args[1] == "-c" || *args[1] == cont){
+        while(menu()){
+            continuous_calculate();
+        }
+    }
+}
+
+bool menu(){
+    printf("Hello would you like to quit? y/n?\n");
+    // scanf();
+
+}
+
+void continuous_calculate(){
+    printf("Enter what values you would like to calculate and then one of the special operands:\n");
+    printf("+, -, /, *, %\n");
+    printf("You can do this across multiple values, ex. 0x10 0xA 0xAB + -\n");
+    printf("Prompt>   ")
 }
