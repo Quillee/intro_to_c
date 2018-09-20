a0681fa81ac2a0c33974619d33e760c79cdd0139
Melvin <peraltamdb@gmail.com>
2018-09-20 00:25:22 -0400

    example 1 complete

--- gen_tutorial/ex01/ex1.c
+++ gen_tutorial/ex01/ex1.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[]){
     // #> this is also a comment
     int distance = 100;
     
-    printf("The distance is, %d\n", distance);
+    printf("The distance is, %d\n", distance)
 
     return 0;
 }
